05c8a0c8f377f5ed6e702f507a2b51aaa2f1225e
Marcin Krysinski <marcin_krysinski@outlook.com>
2024-10-30 14:48:58 +0100

    refactor: simplify env overriding

--- src/spec/setup.spec.ts
+++ src/spec/setup.spec.ts
@@ -1,14 +1,14 @@
-import 'reflect-metadata';
-import { testDbDataSource } from './testDb'
-import { DataSource } from 'typeorm';
+import "reflect-metadata";
+import { testDbDataSource } from "./testDb";
+import { DataSource } from "typeorm";
 
 before(async () => {
   const emptyDataSource = await new DataSource({
-    type: 'postgres',
+    type: "postgres",
     host: process.env.DB_HOST as string,
     username: process.env.DB_USERNAME as string,
     password: process.env.DB_PASSWORD as string,
-    database: '',
+    database: "",
   }).initialize();
 
   const ifNotExist = true;
@@ -20,15 +20,15 @@ before(async () => {
   await testDbDataSource.initialize();
 });
 
-afterEach('clears test database', async () => {
+afterEach("clears test database", async () => {
   await testDbDataSource.transaction(async (manager) => {
-    await manager.query('SET session_replication_role = replica');
+    await manager.query("SET session_replication_role = replica");
 
     for (const entityMeta of testDbDataSource.entityMetadatas) {
       await manager.delete(entityMeta.target, {});
     }
 
-    await manager.query('SET session_replication_role = DEFAULT');
+    await manager.query("SET session_replication_role = DEFAULT");
   });
 });
 
